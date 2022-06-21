7646283dcf2e26e9a0c1870b522bcfea6d27f295
giraffe176 <giraffe176@gmail.com>
2022-06-20 20:37:56 -0500

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,250 +1,261 @@
-var target = args[0]
-var securityThresh = getServerMinSecurityLevel(target) + 5;
-//max server money
-var moneyThresh = getServerMaxMoney(target);
-
-//initial security level
-
-
-//get ram costs of different scripts
-var wRAM = getScriptRam("weaken.script");
-var gRAM = getScriptRam("grow.script")
-var hRAM = getScriptRam("hack.script")
-
-//get hostname of current machine
-var hostname = getHostname()
+/** @param {NS} ns */
+export async function main(ns) {
+	var target = ns.args[0]
+	var securityThresh = ns.getServerMinSecurityLevel(target) + 3;
+	//max server money
+	var moneyThresh = ns.getServerMaxMoney(target) * .99;
 
 
-//manager
+	//initial security level
 
-while (true) {
-	//weaken server to desired level
-	if (getServerSecurityLevel(target) > securityThresh) {
-		var cursecurity = getServerSecurityLevel(target)
-		//number of threads to weaken security to min
-		var wcycle = (cursecurity - securityThresh) / .05
-		//free ram for weaken thread
-		var freeRAMw = getServerMaxRam(hostname) - getServerUsedRam(hostname);
-		//number of threads supported for weaken thread
-		var wgtsupport = freeRAMw / wRAM
-		//number of weaken threads needed 
-		var wgtneeded = wRAM * wcycle
-		//do we have more ram than is needed?
-		if (wgtsupport > wgtneeded) {
-			//if yes, use exact number of threads to weaken
-			var wthreads = Math.ceil(wcycle)
-		}
-		//if no, cap to the max 
-		else { var wthreads = Math.floor(wgtsupport) }
-		toast(wthreads + " Weaken threads")
-		//get time to weaken
-		var wtime = getWeakenTime(target)
-		//execute command on server
-		run("weaken.script", wthreads, target);
-		//wait for weaken script to finish
-		sleep(wtime + 1000);
-		//check to make sure weaken script isn't running
-		var wrun = isRunning("weaken.script", hostname, wthreads, target)
-		//if it is, wait for it...
-		if (wrun == true) { sleep(750) }
-		sleep(500)
 
-	}
-	//grow  
-	else if (getServerMoneyAvailable(target) < moneyThresh) {
+	//get ram costs of different scripts
+	var wRAM = ns.getScriptRam("weaken.script");
+	var gRAM = ns.getScriptRam("grow.script")
+	var hRAM = ns.getScriptRam("hack.script")
+	//random argument to make scripts unique
+	var t = 1
+	var u = 13
+	//random variables 
+	var a = 7
+	var b = 17
+
+	//get hostname of current machine
+	var hostname = ns.getHostname()
+	//manager
+	while (true) {
+		var gtime = ns.getGrowTime(target)
+		//get hack time
+		var htime = ns.getHackTime(target)
 		//get new weaken time
-
-		//if the amount of money on the server is less than half of our goal
-		if (getServerMoneyAvailable(target) <= .5 * moneyThresh) {
-			//find out how much growth is needed
-			var totalgrowth = moneyThresh / getServerMoneyAvailable(target)
-			//SPAM this many growth cycles
-			var gcycle = Math.ceil(growthAnalyze(target, totalgrowth))
-		}
-		else {
-			//run as many growth cycles that are needed.
-			var gcycle = Math.ceil(growthAnalyze(target, 2))
-		}
-		//security increase for all cycles required
+		var wtime = ns.getWeakenTime(target)
+		var freeRAM = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)
+		//This many growth threads are required to double our money
+		var gcycle = Math.floor(ns.growthAnalyze(target, 2))
+		//RAM impact of required growth AND weaken  threads
+		var gRAMneeded = (gRAM * gcycle)
+		//security increase for all growth threads required
 		var gcyclesec = gcycle * .004
 		//number of weaken threads to undo grow increase
 		var wgcycle = Math.ceil(gcyclesec / .05)
+		//RAM impact of needed weaken threads
+		var wRAMneeded = wRAM * wgcycle
+		//weaken server to desired level
+		if (ns.getServerSecurityLevel(target) > securityThresh) {
+			var cursecurity = ns.getServerSecurityLevel(target)
+			//number of threads to weaken security to min
+			var wcycle = (cursecurity - securityThresh) / .05
 
+			//number of threads supported for weaken thread
+			var wgtsupport = freeRAM / wRAM
+			//number of weaken threads needed 
+			var wgtneeded = wRAM * wcycle
+			//do we have more ram than is needed?
+			if (wgtsupport > wgtneeded) {
+				//if yes, use exact number of threads to weaken
+				var wthreads = Math.floor(wcycle)
+			}
+			//if no, cap to the max 
+			else { var wthreads = Math.floor(wgtsupport) }
 
+			//get time to weaken
+			var wtime = ns.getWeakenTime(target)
+			//execute command on server
+			if (wthreads <= 1) { var wthreads = 1 }
+			ns.run("weaken.script", wthreads, target);
+			//wait for weaken script to finish
+			await ns.sleep(wtime + 1000);
+			//check to make sure weaken script isn't ns.running
+			var wrun = ns.isRunning("weaken.script", hostname, wthreads, target)
+			//if it is, wait for it...
+			if (wrun == true) { await ns.sleep(750) }
+			await ns.sleep(500)
+			var t = 1
+			var u = 13
 
-		//how much free ram do we have
-		var freeRAMg = getServerMaxRam(hostname) - getServerUsedRam(hostname);
-		//how much free ram do we need?
-		// total ram needed for weaken threads 
-		var wgRAMneeded = wRAM * wgcycle
-		//Script size times the number of cycles
-		var gRAMneeded = (gRAM * gcycle) + (wgRAMneeded)
-		//RAM impact of weaken cycles
-
-		//actual free ram
-		var afr = freeRAMg - wgRAMneeded
-
-		//do we need more ram than we have available?
-		if (gRAMneeded > afr) {
-			//yes
-			var gthreads = Math.floor((afr / gRAM))
-			toast("IF")
 		}
-		//no
-		else {
-			var gthreads = Math.ceil(gcycle);
-			toast("Else")
-		}
-		//get grow time
-		var gtime = getGrowTime(target)
-		//get weaken time
-		var wtime = getWeakenTime(target)
-		//random variables 
-		var a = 7
-		var b = 17
-		//as negative time is not a thing if grow takes longer than weaken, start grow first
-		if (gtime + 1000 >= wtime) {
-			var wminusgt = gtime - wtime - 1000
-			//run grow script using the number of threads specified in gthreads
-			run("grow.script", gthreads, target, a);
-			sleep(wminusgt)
-			run("weaken.script", wgcycle, target, b);
-			sleep(wtime + 1000);
-
-			a++
-			b++
-		}
-		else {
-			//difference between weaken time and grow time ends 1000ms before weaken does allowing the script to have the same security level
-			var wminusgt = wtime - gtime - 1000
-			run("weaken.script", wgcycle, target, a);
-			//sleep for the difference between weaken and growtime
-			sleep(wminusgt)
-			run("grow.script", gthreads, target), b;
-			sleep(gtime + 1000);
-			a++
-			b++
-		}
-
-
-
-		var wrun = isRunning("weaken.script", hostname, wgcycle, target)
-		var grun = isRunning("grow.script", hostname, gthreads, target)
-		if (wrun == true) { sleep(750) }
-		if (grun == true) { sleep(750) }
-		sleep(500)
-	}
-	//hack
-	else {
-
-
-
-		var hcycle = Math.ceil(.50 / hackAnalyze(target))
-		var whcyclesec = hcycle * .002
-		var whcycle = Math.ceil(whcyclesec / .05)
-		var whRAM = wRAM * whcycle
-		var freeRAMh = getServerMaxRam(hostname) - getServerUsedRam(hostname) - (whRAM);
-		if ((hRAM * hcycle) > (freeRAMh / hRAM)) {
-			var hthreads = Math.floor(freeRAMh / hRAM)
-		}
-		else { var hthreads = Math.ceil(hcycle) }
-		toast(hthreads + "Hack Threads ")
-
-
-
-		// do we have enough free ram to grow AND weaken the server at the same time as our hack and weakens?
-
-		var freeRAMgwh = getServerMaxRam(hostname) - getServerUsedRam(hostname) - whRAM - (hthreads * hRAM)
-		//random argument to make scripts unique
-		var t = 1
-		var u = 13
-
-		//Do we have more than 256 GB available? (CAN BE CHANGED)
-		if (freeRAMgwh > 256) {
-
+		//grow  
+		else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
 			//get new weaken time
-			var wtime = getWeakenTime(target)
-			//get grow time
-			var gtime = getGrowTime(target)
-			//get hack time
-			var htime = getHackTime(target)
-			//CALCULATING COSTS OF GROWING MONEY WHILE HACKING
-			//number of grow cycles required to make back the money we're hacking
-			var hgcycle = Math.ceil(growthAnalyze(target, 2))
-			//amount of security increase for the number of needed grow thread
-			var hgcyclesec = hgcycle * .004
-			//number of weaken threads to undo grow increase
-			var hwgcycle = Math.ceil(hgcyclesec / .05)
+
+			//if the amount of money on the server is less than half of our goal
+			if (ns.getServerMoneyAvailable(target) <= .5 * moneyThresh) {
+				//find out how much growth is needed
+				var totalgrowth = moneyThresh / ns.getServerMoneyAvailable(target)
+				if (totalgrowth <= 1) { totalgrowth = 1.0001 }
+				//SPAM this many growth cycles
+				var gcycle = Math.ceil(ns.growthAnalyze(target, totalgrowth))
+			}
+
+			//how much free ram do we need?
 			// total ram needed for weaken threads 
-			var hwgRAMneeded = wRAM * hwgcycle
-			//total ram needed for grow threads
-			var hgcycleRAMneeded = gRAM * hgcycle + hwgRAMneeded
-			//find out the number of actual grow threads we can support
-			var supportedhgthreads = Math.floor((freeRAMgwh - hwgRAMneeded) / gRAM)
-			//Required number of weaken threads for the number of grow threads we can support
-			var requiredwghthreads = Math.floor(supportedhgthreads / .05)
-			//if the amount of RAM needed to undo is more than we have
-			if (hwgRAMneeded > freeRAMgwh) {
+			var wgRAMneeded = wRAM * wgcycle
+			//Script size times the number of cycles
+			var gRAMneeded = (gRAM * gcycle) + (wRAMneeded)
+			//RAM impact of weaken cycles
+
+			//actual free ram
+			var afr = freeRAM - wgRAMneeded
+
+			//do we need more ram than we have available?
+			if (gRAMneeded > afr) {
 				//yes
-				var hgthreads = Math.floor((freeRAMgwh / gRAM))
-				toast("IF")
+				var gthreads = Math.floor((afr / gRAM))
+
 			}
 			//no
 			else {
-				var hgthreads = Math.ceil(hgcycle);
-				toast("Else")
+				var gthreads = Math.floor(gcycle);
+
+			}
+			//get grow time
+			var gtime = ns.getGrowTime(target)
+			//get weaken time
+			var wtime = ns.getWeakenTime(target)
+			var freeRAM = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)
+			if (gthreads <= 1) { var gthreads = 1 }
+			if (wgcycle <= 1) { var wgcycle = 1 }
+			ns.run("grow.script", gthreads, target, a);
+			if (wgRAMneeded + gRAMneeded < freeRAM) {
+				ns.run("weaken.script", wgcycle, target, b)
 			}
 
-			//difference between weaken time and grow time
-			var wminusgtime = wtime - gtime
-			//difference between sum of HACK and GROW and weaken
-			var wait = (htime + gtime) - wtime
-			//difference between weaken and hack
-			var wminushtime = wtime - htime
-			//difference between grow and hack
-			var gminush = gtime - htime
+			//ns.run grow script using the number of threads specified in gthreads
 
+			await ns.sleep(wtime + 1000);
+
+			a++
+			b++
+		
+
+
+
+
+		var wrun = ns.isRunning("weaken.script", hostname, wgcycle, target)
+		var grun = ns.isRunning("grow.script", hostname, gthreads, target)
+		if (wrun == true) { await ns.sleep(750) }
+		if (grun == true) { await ns.sleep(750) }
+		await ns.sleep(500)
+		var t = 1
+		var u = 13
+	}
+	//hack
+	else {
+		//how many threads are required to hack half the server's money?
+		var hcycle = Math.ceil(.50 / ns.hackAnalyze(target))
+		//security increase of all these threads
+		var hcyclesec = hcycle * .002
+		//RAM impact of hack
+		var hRAM = hRAM * hcycle
+		//how many weaken cycles are required to undo the hack
+		var whcycle = Math.floor(hcyclesec / .05)
+		//RAM impact of this
+		var whRAM = wRAM * whcycle
+		//RAM LEFT OVER for hacking =
+		var freeRAMh = freeRAM - whRAM;
+		if (((hRAM + whRAM) > (freeRAMh / hRAM))) {
+			var hthreads = Math.floor(freeRAMh / hRAM)
+		}
+		else { var hthreads = hcycle }
+
+		//Total RAM impact
+		var totalramimpact = hthreads * hRAM + whRAM
+		var freeRAM = ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname)
+		//Left over RAM
+		var loRAM = freeRAM - totalramimpact
+
+		//Do we have more than 256 GB available? (CAN BE CHANGED)
+		if (loRAM > 256) {
+			var gcycle = Math.floor(ns.growthAnalyze(target, 2))
+			//RAM impact of needed weaken threads
+			var wRAMneeded = wRAM * wgcycle
+			//total ram needed for grow threads
+			var hgcycleRAMneeded = gRAM * gcycle + wRAMneeded
+			//find out the number of actual grow only threads we can support
+			var supportedhgthreads = Math.floor(loRAM / gRAM)
+			//Required number of weaken threads for the number of grow threads we can support
+			var requiredwghthreads = Math.ceil(supportedhgthreads / .05)
+			//RAM impact of these theoretical weaken threads
+			var ramw = requiredwghthreads * wRAM
+			//free RAM for grow
+			var freeRAMg = loRAM - ramw
+			//max supported number of grow threads
+			var actualgrow = Math.floor(freeRAMg / gRAM)
+
+			//if the required amount of ram is more than we have, use this number of threads
+			if (hgcycleRAMneeded > loRAM) {
+				//yes
+				var hgthreads = actualgrow
+
+			}
+			//no
+
+			else {
+				var hgthreads = gcycle;
+
+			}
+
+			//how much did the security go up from that growth?
+			var hgcyclesec = hgthreads * .004
+
+			var anumweaken = Math.ceil(hgcyclesec / .05)
 			//start both weaken scripts
 			//Order is going to be Hack,Weaken,Grow,Weaken
-			run("weaken.script", hwgcycle, target, t)
-			//wait a second to give the system a breather
-			sleep(1250)
-			run("weaken.script", whcycle, target, u);
+			if (anumweaken <= 1) { var anumweaken = 1 }
+			if (whcycle <= 1) { var whcycle = 1 }
+			if (hgthreads <= 1) { var hgthreads = 1 }
+			if (hthreads <= 1) { var hthreads = 1 }
+
+			//get new weaken time
+			//get hack time
+			//difference between weaken time and grow time
+
 			//Weaken = 4x Grow =3.2X Hack=1X
 			//remove that second so that grow and hack end when the first weaken script does
-			sleep(wminusgtime - 1050);
-			run("grow.script", hgthreads, target, t)
+			//wait a second to give the system a breather
 			//remove a little bit of time so that hack comes first
-			sleep(gminush-500)
-			run("hack.script", hthreads, target, u);
-			
+			ns.run("weaken.script", whcycle, target, t, "hac")
+			await ns.sleep(500)
+			ns.run("weaken.script", anumweaken, target, u, "gro");
+			var gtime = ns.getGrowTime(target)
+			var wtime = ns.getWeakenTime(target)
+			var wgtime = wtime - gtime
+			await ns.sleep(wgtime - 250);
+			ns.run("grow.script", hgthreads, target, t)
+			var gtime = ns.getGrowTime(target)
+			var htime = ns.getHackTime(target)
+			var hgtime = gtime - htime
+			await ns.sleep(hgtime - 500)
+			ns.run("hack.script", hcycle, target, u);
+
+
+			await ns.sleep(htime + 500)
+			if (ns.isRunning("weaken.script", ns.getHostname(), target, u) || ns.isRunning("weaken.script", ns.getHostname(), target, t) || ns.scriptRunning("grow.script", ns.getHostname(), target, t) || ns.scriptRunning("hack.script", ns.getHostname(), target, u) == true) { await ns.sleep(1000) }
 			t++;
 			u++;
-			toast("Success")
-			sleep(htime+5000)
 		}
 		else {
 
 			//get grow time
-			var gtime = getGrowTime(target)
-			//get hack time
-			var htime = getHackTime(target)
-			//get new weaken time
-			var wtime = getWeakenTime(target)
-
+			var t = 1
+			var u = 13
+			var wtime = ns.getWeakenTime(target)
 			var wminught = wtime - htime - 1000
 
 			//difference between weaken time and hack time ends 1000ms before weaken does allowing the script to have the same security level
 			var wminusht = wtime - htime - 350
-			run("weaken.script", whcycle, target, u);
-			//sleep for the difference between weaken and hack time
-			sleep(wminusht)
+			if (whcycle <= 1) { var whcycle = 1 }
+			if (hthreads <= 1) { var hthreads = 1 }
+			ns.run("weaken.script", whcycle, target, u);
+			//await ns.sleep for the difference between weaken and hack time
+			await ns.sleep(wminusht)
 			//then hack
-			run("hack.script", hthreads, target, u);
-			++u
-			toast("failure")
-			sleep(htime+5000)
+			ns.run("hack.script", hthreads, target, u);
+			u++
+
+			await ns.sleep(htime + wtime / 26)
 
 		}
 	}
+}
 }
