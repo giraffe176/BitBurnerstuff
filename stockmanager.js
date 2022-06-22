11a00d0ec8a617356f55a75f611c3780d2f03e6a
giraffe176 <giraffe176@gmail.com>
2022-06-21 19:51:18 -0500

    Create stockmanager.js

---
+++ stockmanager.js
@@ -0,0 +1,51 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	while (true) {
+		for (const s of ns.stock.getSymbols()) {
+			var forcast = ns.stock.getForecast(s)
+			if (forcast >= .6) {
+				var k = 1;
+				var p = 1
+			}
+			if (forcast <= .4) {
+				var k = -1
+				var p = -1
+			}
+			if (.4 < forcast < .6) {
+				var k = 0
+				var p = 0
+			}
+			var pos = ns.stock.getPosition(s)
+			var shares = pos[0]
+			var maxshares = ns.stock.getMaxShares(s)
+			var maxmoney = ns.getServerMoneyAvailable("home") - 100000
+			if (k == 1 && p == -1) {
+				if (shares < maxshares - 1) {
+
+					var sharesneeded = maxshares - shares
+					var bidprice = ns.stock.getBidPrice(s)
+					if (sharesneeded * bidprice < maxmoney) {
+						ns.stock.buy(s, sharesneeded)
+
+					}
+					else (ns.stock.buy(s, maxmoney / bidprice))
+				}
+			}
+			if (k == -1) {
+				var pricepaid = pos[1]
+				var currentprice = ns.stock.getAskPrice(s)
+
+				if (shares > 0) {
+					var profit = (currentprice * shares) - (pricepaid * shares)
+					var percentgain = currentprice / pricepaid
+					ns.tprint("Shares sold for $", profit, "     ", percentgain)
+					ns.stock.sell(s, shares)
+
+				}
+
+
+			}
+		}
+		await ns.sleep(1000)
+	}
+}
